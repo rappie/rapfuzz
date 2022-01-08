8b05505973124bcab3a392d34b6d5ed19f9262bf
Rappie <jasper@rappie.com>
2022-01-08 14:54:44 +0100

    Logging to file

--- rapfuzz.py
+++ rapfuzz.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import pdb
+import datetime
 import argparse
+from urllib.parse import urlparse
 from collections import defaultdict
 
 import wfuzz
@@ -10,29 +12,48 @@ import wfuzz
 RESULT_FILTER_CEILING = 3
 
 
-def fuzz(urlList, wordlist, outputDir):
+def fuzz(urlList, wordlist, outputDir="."):
 
-    url = "http://testphp.vulnweb.com/FUZZ"
-    sc = [200, 204, 301, 302, 307, 308, 401, 403, 405, 500]
-    hc = []
-    payloads = [("file", dict(fn="wordlist.txt"))]
+    for url in urlList:
 
-    resultData = defaultdict(list)
+        u = urlparse(url)
+        scheme = u.scheme
+        netloc = u.netloc
+        path = u.path.replace("/", "_")
+        now = (
+            str(datetime.datetime.now())
+            .replace(" ", "_")
+            .replace(":", "-")
+            .split(".")[0]
+        )
+        logFileName = f"rapfuzz.log.{scheme}_{netloc}{path}.{now}"
 
-    with wfuzz.FuzzSession(url=url, payloads=payloads) as session:
-        for resultLine in session.fuzz(sc=sc, hc=hc):
-            resultData[resultLine.code].append(resultLine)
+        with open(logFileName, "w") as logFile:
 
-    for code, resultList in resultData.items():
+            def log(logString=""):
+                print(logString)
+                logFile.write(f"{logString}\n")
 
-        if len(resultList) >= RESULT_FILTER_CEILING:
-            print(f"too many results for code: {code} ({len(resultList)})")
+            sc = [200, 204, 301, 302, 307, 308, 401, 403, 405, 500]
+            hc = []
+            payloads = [("file", dict(fn=wordlist))]
 
-        else:
-            for resultLine in resultList:
-                print(resultLine)
+            resultData = defaultdict(list)
 
-        print()
+            with wfuzz.FuzzSession(url=url, payloads=payloads) as session:
+                for resultLine in session.fuzz(sc=sc, hc=hc):
+                    resultData[resultLine.code].append(resultLine)
+
+            for code, resultList in resultData.items():
+
+                if len(resultList) >= RESULT_FILTER_CEILING:
+                    log(f"too many results for code: {code} ({len(resultList)})")
+
+                else:
+                    for resultLine in resultList:
+                        log(resultLine)
+
+                log()
 
 
 def parseArgs():
